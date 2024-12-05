6eb45b917bd24f792872f5f77729a52fa7caa2e3
André Vinícius <avnc@cin.ufpe.br>
2024-12-05 10:13:21 -0300

    Correção de comentários

--- Protótipo V3/api_calling.py
+++ Protótipo V3/api_calling.py
@@ -32,7 +32,7 @@ def get_game_data(game_name, API_KEY=GIANT_BOMB_API):
     param game_name (string): Nome do jogo a ser pesquisado
     return games (list): Lista de resultados de pesquisa, cada resultado é um dicionário de informações sobre um jogo com as seguintes keys:
     ['aliases', 'api_detail_url', 'date_added', 'date_last_updated', 'deck', 'description', 'guid', 'id', 'image', 'image_tags', 'name', 'site_detail_url', 'resource_type']
-    image_tags é também um dicionário com as keys: ['icon_url', 'medium_url', 'screen_url', 'screen_large_url', 'small_url', 'super_url', 'thumb_url', 'tiny_url', 'original_url', 'image_tags']
+    image é também um dicionário com as keys: ['icon_url', 'medium_url', 'screen_url', 'screen_large_url', 'small_url', 'super_url', 'thumb_url', 'tiny_url', 'original_url', 'image_tags']
     """
 
     url = f'https://www.giantbomb.com/api/search/?api_key={API_KEY}&format=JSON&query={game_name}'
@@ -56,7 +56,7 @@ def url_to_png(mode, data, file_name):
         url = f'https://image.tmdb.org/t/p/original/{path}'
     
     elif mode == 'game':
-        url = f'{data.get('image', ).get('original_url')}'
+        url = f'{data.get('image', {}).get('original_url')}'
     
     response = requests.get(url)
     if response.status_code == 200:
